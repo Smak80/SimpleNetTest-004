fd64c663a2743391dfa7c43be4f4c8745befd0cf
Sergey Makletsov <smaklets@kpfu.ru>
2023-03-02 08:52:38 +0300

    Версия с параллельным подключением нескольких клиентов

--- Server/src/Server.kt
+++ Server/src/Server.kt
@@ -1,9 +1,9 @@
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.io.PrintWriter
-import java.lang.Exception
 import java.net.ServerSocket
-import java.net.Socket
+import kotlin.Exception
+import kotlin.concurrent.thread
 
 class Server {
     companion object{
@@ -12,28 +12,37 @@ class Server {
 
     fun start(){
         val ss = ServerSocket(PORT)
-        var br: BufferedReader? = null
-        var pw: PrintWriter? = null
-        var s: Socket? = null
         try {
             while (true) {
-                s = ss.accept()
-                val iStream = s.getInputStream()
-                br = BufferedReader(InputStreamReader(iStream))
-                val inputString = br.readLine()
-                println(inputString)
-                val oStream = s.getOutputStream()
-                pw = PrintWriter(oStream)
-                pw.println("Привет от сервера в ответ!")
-                pw.flush()
+                val s = ss.accept()
+                thread {
+                    var br: BufferedReader? = null
+                    var pw: PrintWriter? = null
+                    try {
+                        println("Подключен очередной клиент")
+                        val iStream = s.getInputStream()
+                        br = BufferedReader(InputStreamReader(iStream))
+                        val inputString = br.readLine()
+                        println(inputString)
+                        Thread.sleep(20000)
+                        val oStream = s.getOutputStream()
+                        pw = PrintWriter(oStream)
+                        pw.println("Привет от сервера в ответ!")
+                        pw.flush()
+                    } catch (e: Exception){
+                        println("Что-то пошло не так... ")
+                        println(e)
+                    } finally {
+                        pw?.close()
+                        br?.close()
+                        s?.close()
+                    }
+                }
             }
         } catch (e: Exception){
             println("Что-то пошло не так... ")
             println(e)
         } finally {
-            pw?.close()
-            br?.close()
-            s?.close()
             ss.close()
         }
     }
